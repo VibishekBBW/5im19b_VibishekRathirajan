d3c6a4c32f781d9eb11b2745a430e5724f98a007
VibishekBBW <77846152+VibishekBBW@users.noreply.github.com>
2021-01-27 18:51:45 +0100

    Update MyEchoServer.java

--- EchoServer/src/MyEchoServer.java
+++ EchoServer/src/MyEchoServer.java
@@ -18,7 +18,7 @@ public class MyEchoServer extends Thread {
 	
 	public static void main(String[] args) throws IOException {
 		
-		String version = new String( "MyEchoServer - Version 1.1.0" );
+		String version = new String( "MyEchoServer - Version 1.0.0" );
 		
 		ServerSocket serverSocket = null;
 
